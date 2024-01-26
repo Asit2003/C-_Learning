e6386ab55133504e0eb084b43805b3b5711592cf
Asit2003 <78712740+Asit2003@users.noreply.github.com>
2024-01-26 06:28:22 +0000

    update few codes

--- .ccls-cache/@home@runner@DSAC/Day5@IOManipulation.cpp
+++ .ccls-cache/@home@runner@DSAC/Day5@IOManipulation.cpp
@@ -1,38 +1,5 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//   int a = 30, b = 78;
-//   bool c = true;
-//   cout << c << '\n';
-//   cout << std::boolalpha;
-//   cout << c << '\n';
-//   cout << noboolalpha;
-//   cout << c << '\n';
-
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-//   cout << std::hex;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-//   cout << std::oct;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-//   cout << std::dec;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-
-//   cout << std::showbase;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-//   cout << std::showpos;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-//   cout << std::uppercase;
-//   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-
-//   cout << std ::setw(8);
-//   cout << std ::setfill('*');
-//   cout << a << '\n';
-
-//   return 0;
-// }
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -62,12 +29,21 @@ int main() {
   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
   cout << std::uppercase;
   cout << "The value of a is " << a << " and the value of b is " << b << '\n';
-  cout<< nouppercase;
-  cout<< noshowpos;
+  cout << nouppercase;
+  cout << noshowpos;
 
   // Output with width and fill
   cout << std::setw(8);
   cout << std::setfill('*');
+  cout << a << '\n';
+  cout << left;
+  cout << std::setw(8);
+  cout << a << '\n';
+  cout << right;
+  cout << std::setw(8);
+  cout << a << '\n';
+  cout << internal;
+  cout << std::setw(8);
   cout << a << '\n';
 
   return 0;
